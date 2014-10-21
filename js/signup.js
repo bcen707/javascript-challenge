9b01c8f20acf084394e37d68853a5903de2f63f4
bcen707 <czarsalad707@gmail.com>
2014-10-21 14:21:38 -0700

    started...

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,13 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+"use strict";
+
+document.addEventListener("")
+
+for (i = 0; i < usStates.length; i++) {
+	var option = 
+}
+
+
