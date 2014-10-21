4f2ab5873d242b601fd16e2a0de6f528f432f49b
bcen707 <bcen707@lt-ougl-3-24.eplt.washington.edu>
2014-10-21 15:34:18 -0700

    started coding functions

--- js/signup.js
+++ js/signup.js
@@ -1,14 +1,30 @@
 /*
+	Billy Cen
+	INFO 343
+	Assignment 4
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
 
 "use strict";
 
-document.addEventListener("")
+document.addEventListener("click", DOMContentLoaded);
 
-for (i = 0; i < usStates.length; i++) {
-	var option = 
+document.addEventListener("click", change);
+
+
+function DOMContentLoaded {
+	for (i = 0; i < usStates.length; i++) { 			  // Loop through state array
+		var option = usStates[i].createElement("OPTION"); // Give each state a new option element
+		var setElement = document.getElementById("option").appendChild(usStates[i]);
+	}	
 }
+
+function change {
+
+}
+
+
+
 
 
