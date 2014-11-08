94b14e7d00d39d3e9bd20e1f6c195abcf94ec721
bcen707 <bcen707@lt-ougl-3-06.eplt.washington.edu>
2014-11-07 18:14:10 -0800

    almost done

--- js/signup.js
+++ js/signup.js
@@ -10,23 +10,46 @@
 
 
 function loadStates() {
-
-	var getStates = document.getElementsByName("state"); // Accesses the us-states array
-	for (var i = 0; i < getStates.length; i++) { 			  // Loop through state array
+	var getStates = document.getElementsByName("state")[0]; // Accesses the us-states array
+	
+	for (var i = 0; i < usStates.length; i++) { 			  // Loop through state array
 		var option = document.createElement("option");
-		option.value = getStates[i].code;
-		var text = document.createTextNode(getStates[i].name);
+		option.value = usStates[i].code;
+		var text = document.createTextNode(usStates[i].name);
 		option.appendChild(text);
 		getStates.appendChild(option);
 	}
-		//var elem = document.getElementById(getStates[i].code) 
+
+		
+}
+
+	
+function hideShowOccup() {
+	var occup = document.getElementsByName("occupationOther")[0];
+	if (occupation.value != "other") {
+		occup.style.display = "none";
+		occup.value = '';
+	} else { 
+		occup.style.display = "inline";
+	}	
+
+}
+
+function leavePage() {
+	var exit = window.confirm("Are you sure you want to leave this amazing site?")
+	if (exit == true) {
+		window.location = 'http://google.com';
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	loadStates();
+	occupation.addEventListener('change', hideShowOccup);
+	cancelButton.addEventListener('click', leavePage);
+});
+
 
 		var myForm = document.getElementById("signup");
 		// myForm.addEventListener('submit', onSubmit);
-}
-
-document.addEventListener("DOMContentLoaded", loadStates);
-	
-	document.write('code reached here');
 
 
